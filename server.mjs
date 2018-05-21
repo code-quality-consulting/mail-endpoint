69aff49c12fac848e3d012138a98908e30314c46
zoverlvx <zoverhulser@gmail.com>
2018-05-21 15:51:52 -0400

    Refactors asynchronicity with parseq.

--- server.mjs
+++ server.mjs
@@ -4,22 +4,26 @@
 
 import http from "http";
 
-const server = http.createServer(function (req, res) {
-    let body = [];
-    req.on("error", (err) => console.err(err))
-        .on("data", function (chunk) {
-            body.push(chunk);
-            console.log(body);
-        }).on("end", function () {
-            body = Buffer.concat(body).toString();
-            console.log(body);
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-                "X-Powered-By": "cqc"
+function serverRequestor(callback) {
+    const server = http.createServer(function (req, res) {
+        let body = [];
+        req.on("error", (err) => console.err(err))
+            .on("data", function (chunk) {
+                body.push(chunk);
+                console.log(body);
+            }).on("end", function () {
+                body = Buffer.concat(body).toString();
+                console.log(body);
+                res.writeHead(200, {
+                    "Content-Type": "application/json",
+                    "X-Powered-By": "cqc"
+                });
+                res.write(body);
+                res.end();
             });
-            res.write(body);
-            res.end();
-        });
-}).listen(3001);
+    }).listen(3001, function () {
+        callback(server);
+    });
+}
 
-export default server;
+export {serverRequestor};
