a9ffa0479fd66fa8295c84343a01fa0f004d9b6d
zoverlvx <zoverhulser@gmail.com>
2018-05-21 15:51:52 -0400

    Refactors asynchronicity with parseq.

--- tests/request.mjs
+++ tests/request.mjs
@@ -3,42 +3,56 @@
 */
 import assert from "assert";
 import http from "http";
-import server from "../server";
+import parseq from "../dependencies/parseq.mjs";
+import {serverRequestor} from "../server";
 
-const postData = JSON.stringify({
-    "msg": "Hello world!"
-});
-
-const options = {
-    hostname: "127.0.0.1",
-    port: 3001,
-    path: "/email-address",
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(postData)
+function testRequest(value, reason) {
+    if (reason) {
+        console.error(reason);
     }
-};
-
-const req = http.request(options, function (res) {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding("utf8");
-    let data = "";
-    res.on("data", function (chunk) {
-        console.log("Here's a chunk: ", chunk);
-        data += chunk;
-    });
-    res.on("end", function () {
-        assert.strictEqual(data, `{"msg":"Hello world!"}`);
+    if (value) {
+        assert.strictEqual(value, `{"msg":"Hello world!"}`);
         console.log("Success");
-        server.close(() => console.log("Server closed."));
-    });
-});
-req.on("error", (e) => console.error(`Problem with request: ${e.message}`));
-req.setTimeout(5000, function () {
-    console.error("No response.");
-});
-req.write(postData);
-req.end();
+    }
+}
 
+function testRequestor(callback, server) {
+    const postData = JSON.stringify({
+        "msg": "Hello world!"
+    });
+
+    const options = {
+        hostname: "127.0.0.1",
+        port: 3001,
+        path: "/email-address",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(postData)
+        }
+    };
+
+    const req = http.request(options, function (res) {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding("utf8");
+        let data = "";
+        res.on("data", function (chunk) {
+            console.log("Here's a chunk: ", chunk);
+            data += chunk;
+        });
+        res.on("end", function () {
+            callback(data);
+            server.close(() => console.log("Server closed."));
+        });
+    });
+    req.on("error", (e) => console.error(`Problem with request: ${e.message}`));
+    req.setTimeout(5000, function () {
+        console.error("No response.");
+    });
+    req.write(postData);
+    req.end();
+
+}
+
+parseq.sequence([serverRequestor, testRequestor])(testRequest);
