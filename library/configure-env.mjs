eca4fa499eb2dcd30cf6fff1739027854bfb16eb
Zach Overhulser <zoverhulser@gmail.com>
2018-06-26 20:46:18 -0500

    Checks authorized environment variables.

--- library/configure-env.mjs
+++ library/configure-env.mjs
@@ -1,5 +1,12 @@
-function checkEnvIntruders() {
-   return true; 
+function checkEnvIntruders(envVariables, allowedVariables = []) {
+    const envNames = Object.keys(envVariables);
+    const unauthorizedVariables = envNames.filter((envVariableName) =>
+        !allowedVariables.includes(envVariableName)
+    );
+    if (unauthorizedVariables.length === 0) {
+        return false;
+    }
+    return true; 
 }
 
 function configureEnv() {
