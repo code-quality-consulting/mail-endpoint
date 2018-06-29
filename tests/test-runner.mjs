9c1fc99436fb70762e13611594104fd563a0ca81
Zach Overhulser <zoverhulser@gmail.com>
2018-06-28 20:15:51 -0500

    Includes authorized environment variables.

--- tests/test-runner.mjs
+++ tests/test-runner.mjs
@@ -1,5 +1,10 @@
+/*jslint
+    node
+*/
+
 import testPostServer from "./request";
 import testEnvVariables from "./test-env";
 
 testPostServer();
 testEnvVariables();
+console.log("Here is the new environment and its variables: ", process.env);
