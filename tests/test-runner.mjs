b4154f7fa894a5b72a121cb9cd783d3c995b104a
Zach Overhulser <zoverhulser@gmail.com>
2018-07-03 17:50:31 -0500

    Passes environment variables toserver and request.

--- tests/test-runner.mjs
+++ tests/test-runner.mjs
@@ -5,6 +5,6 @@
 import testPostServer from "./request";
 import testEnvVariables from "./test-env";
 
-testPostServer({PORT: process.env.PORT});
+testPostServer({PORT: process.env.PORT, HOST: process.env.HOST});
 testEnvVariables();
 console.log("Here is the new environment and its variables: ", process.env);
