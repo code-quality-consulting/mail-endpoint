86d5a84513e5742495f01c844f9f14e07261d758
Zach Overhulser <zoverhulser@gmail.com>
2018-07-02 22:48:01 -0500

    Isolates environment variables for server.

--- server.mjs
+++ server.mjs
@@ -4,26 +4,28 @@
 
 import http from "http";
 
-function serverRequestor(callback) {
-    const server = http.createServer(function (req, res) {
-        let body = [];
-        req.on("error", (err) => console.err(err))
-            .on("data", function (chunk) {
-                body.push(chunk);
-                console.log(body);
-            }).on("end", function () {
-                body = Buffer.concat(body).toString();
-                console.log(body);
-                res.writeHead(200, {
-                    "Content-Type": "application/json",
-                    "X-Powered-By": "cqc"
+function makeServer(environmentVariables) {
+    return function serverRequestor(callback) {
+        const server = http.createServer(function (req, res) {
+            let body = [];
+            req.on("error", (err) => console.err(err))
+                .on("data", function (chunk) {
+                    body.push(chunk);
+                    console.log(body);
+                }).on("end", function () {
+                    body = Buffer.concat(body).toString();
+                    console.log(body);
+                    res.writeHead(200, {
+                        "Content-Type": "application/json",
+                        "X-Powered-By": "cqc"
+                    });
+                    res.write(body);
+                    res.end();
                 });
-                res.write(body);
-                res.end();
-            });
-    }).listen(3001, function () {
-        callback(server);
-    });
+        }).listen(environmentVariables.PORT, function () {
+            callback(server);
+        });
+    };
 }
 
-export {serverRequestor};
+export {makeServer};
