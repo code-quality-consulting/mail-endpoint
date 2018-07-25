8f2f52184100475288b4d0ddaefe1e8a478b29b0
Zach Overhulser <zoverhulser@gmail.com>
2018-07-25 13:57:50 -0500

    Encapusulates email server post.

--- tests/api-post-request.mjs
+++ tests/api-post-request.mjs
@@ -1,50 +1,52 @@
 /*jslint
     node
 */
-import assert from "assert";
 import https from "https";
-import parseq from "../dependencies/parseq.mjs";
-import {makeServer} from "../server";
-const {ML_API_KEY, CQC_GROUP_ID} = process.env;
 
-const postData = JSON.stringify({
-    email: "demo@gmail.com",
-    name: "Zach and Ben",
-    fields: {
-        company: "MailerLite"
-    }
-});
+function registerEmail(environmentVariables) {
+    return function registrationRequestor(callback) {
+        const postData = JSON.stringify({
+            email: "demo@gmail.com",
+            name: "Zach and Ben",
+            fields: {
+                company: "MailerLite"
+            }
+        });
 
-const options = {
-    hostname: "api.mailerlite.com",
-    path: `/api/v2/groups/${CQC_GROUP_ID}/subscribers`,
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(postData),
-        "X-MailerLite-ApiKey": ML_API_KEY
-    }
-};
-console.log(`api-post-request.mjs:28: Value of path: ${options.path}`);
-const req = https.request(options, function (res) {
-    res.setEncoding("utf8");
-    let data = "";
-    res.on("data", function (chunk) {
-        data += chunk;
-    });
-    res.on("end", function () {
-        let subscriberInfo = data;
-        console.log(JSON.parse(subscriberInfo));
-    });
-});
+        const {PORT, HOST, ML_API_KEY, CQC_GROUP_ID} = environmentVariables;
+        const options = {
+            hostname: HOST,
+            port: PORT,
+            path: `/api/v2/groups/${CQC_GROUP_ID}/subscribers`,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": Buffer.byteLength(postData),
+                "X-MailerLite-ApiKey": ML_API_KEY
+            }
+        };
+        const req = https.request(options, function (res) {
+            res.setEncoding("utf8");
+            let data = "";
+            res.on("data", function (chunk) {
+                data += chunk;
+            });
+            res.on("end", function () {
+                let subscriberInfo = data;
+                callback(JSON.parse(subscriberInfo));
+            });
+        });
 
-req.on("error", (e) => console.error(`Problem with request: ${e.message}`));
+        req.on("error", (e) => console.error(`Problem with request: ${e.message}`));
 
-req.setTimeout(5000, function () {
-    console.error("No response");
-});
+        req.setTimeout(5000, function () {
+            console.error("No response");
+        });
 
-req.write(postData);
+        req.write(postData);
 
-req.end();
+        req.end();
+    };
+}
 
+export default registerEmail;
--- tests/test-runner.mjs
+++ tests/test-runner.mjs
@@ -5,9 +5,11 @@
 import testPostServer from "./client-post-request";
 import testGetServer from "./test-get";
 import testEnvVariables from "./test-env";
-const {CQC_PORT, CQC_HOST, ML_PORT, ML_HOST} = process.env;
+import registerEmail from "./api-post-request";
+const {CQC_GROUP_ID, ML_API_KEY, CQC_PORT, CQC_HOST, ML_PORT, ML_HOST} = process.env;
 
 testGetServer({PORT: ML_PORT, HOST: ML_HOST});
 testPostServer({PORT: CQC_PORT, HOST: CQC_HOST});
+registerEmail({PORT: ML_PORT, HOST: ML_HOST, CQC_GROUP_ID, ML_API_KEY});
 testEnvVariables();
 console.log("Here is the new environment and its variables: ", process.env);
