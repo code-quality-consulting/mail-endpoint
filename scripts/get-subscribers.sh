40cb37e8a68bb8c9a86ce81c217ba0426bdced29
Mike Ricos <mike.ricos@gmail.com>
2018-07-25 11:23:44 -0500

    Tests GETting subscribers from email group.

---
+++ scripts/get-subscribers.sh
@@ -0,0 +1,4 @@
+## Will get subscribers based on id of group
+## CQC group id is 9753280
+curl -v http://api.mailerlite.com/api/v2/groups/9753280/subscribers \
+-H "X-MailerLite-ApiKey: 3c4ca8904371ef697f6d306d75d1d463"
