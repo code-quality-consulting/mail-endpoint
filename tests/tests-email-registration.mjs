4774935390d7d7d3e5fb27bc555aa95ae7041b0b
Zach Overhulser <zoverhulser@gmail.com>
2018-08-17 12:23:55 -0500

    Recomposes delete request.

--- tests/tests-email-registration.mjs
+++ tests/tests-email-registration.mjs
@@ -4,8 +4,10 @@
 import assert from "assert";
 import checkEmailAbsence from "./check-email-absence";
 import registerEmail from "./api-post-request";
+import deleteEmail from "./api-delete-request";
 import parseq from "../dependencies/parseq";
-// import DELETE re
+
+const {ML_HOST, ML_PORT, ML_API_KEY} = process.env;
 
 function emailRegistrationAssertions(value, reason) {
     if (reason) {
@@ -13,10 +15,22 @@ function emailRegistrationAssertions(value, reason) {
     }
     if (value) {
         assert.strictEqual(value.email, "demo@cqc.com");
-        console.log("Request successfully gets.");
     }
+    // then deletes email
+    deleteEmail({PORT: ML_PORT, HOST: ML_HOST, ML_API_KEY})(
+        function (value, reason) {
+            if (reason) {
+                console.error(reason);
+            }
+            if (value) {
+                console.log(value);
+            }
+        }
+    );
 }
 
+// checks for email with GET
+// then posts email
 function testEmailRegistration(environmentVariables) {
     return parseq.sequence([
         checkEmailAbsence(environmentVariables),
