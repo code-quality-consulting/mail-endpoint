301f539c426c0e48bb04380abd38980911498232
Zach Overhulser <zoverhulser@gmail.com>
2018-08-21 19:33:10 -0500

    Tests assertions made in client post request.

--- tests/client-post-request.mjs
+++ tests/client-post-request.mjs
@@ -1,22 +1,55 @@
 /*jslint
     node
 */
-import assert from "assert";
 import http from "http";
 import parseq from "../dependencies/parseq.mjs";
+import assertionLogger from "./assertion-logger";
 import {makeServer} from "../server";
 
+/* maybe use for testing the payload
+function checkPostRequest(request, payload = []) {
+    console.log("here is request: ", request);
+    const postBody = Object.keys(request);
+    console.log("here is post body: ", postBody);
+    const unauthorizedData = postBody.filter((data) =>
+        !payload.includes(data)
+    );
+    console.log("unauthorized data: ", unauthorizedData);
+    if (unauthorizedData.length === 0) {
+        return false;
+    }
+    return true;
+}
+*/
+
 function testRequest(value, reason) {
-    if (reason) {
-        console.error(reason);
-    }
-    if (value) {
-        assert.strictEqual(value.email, "pseudouser@pseudodomains.com");
-        assert.strictEqual(value.groups[0], "tdd");
-        assert.strictEqual(value.firstName, "Pseudo");
-        assert.strictEqual(value.lastName, "User");
-        console.log("Client request successfully posts.");
-    }
+    const assertions = {
+        "Incorrect email address": {
+            expect: typeof(value.email) === "string"
+                    && value.email === "pseudouser@pseudodomains.com",
+            toEqual: true,
+            actualValue: value.email
+        },
+        "Incorrect group": {
+            expect: typeof(value.groups[0]) === "string"
+                    && value.groups[0] === "tdd",
+            toEqual: true,
+            actualValue: value.groups[0]
+        },
+        "Incorrect first name": {
+            expect: typeof(value.firstName) === "string"
+                    && value.firstName === "Pseudo",
+            toEqual: true,
+            actualValue: value.firstName
+        },
+        "Incorrect last name": {
+            expect: typeof(value.lastName) === "string"
+                    && value.lastName === "User",
+            toEqual: true,
+            actualValue: value.lastName
+        }
+    };
+    assertionLogger(assertions, reason, value);
 }
 
 function makeTester(environmentVariables) {
