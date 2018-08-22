23017fab1a3c3fca7b7ca04e1e8d0b7b158b5ac0
Zach Overhulser <zoverhulser@gmail.com>
2018-08-21 20:50:22 -0500

    Asserts value types and content of the post payload to ML.

--- tests/tests-email-registration.mjs
+++ tests/tests-email-registration.mjs
@@ -1,21 +1,49 @@
 /*jslint
     node
 */
-import assert from "assert";
 import checkEmailAbsence from "./check-email-absence";
 import registerEmail from "./api-post-request";
 import deleteEmail from "./api-delete-request";
 import parseq from "../dependencies/parseq";
-
+import assertionLogger from "./assertion-logger";
 const {ML_HOST, ML_PORT, ML_API_KEY} = process.env;
 
 function emailRegistrationAssertions(value, reason) {
-    if (reason) {
-        console.error(reason);
-    }
-    if (value) {
-        assert.strictEqual(value.email, "demo@cqc.com");
-    }
+    const assertions = {
+        "It should generate an id": {
+            expect: value.id === 283325272034485980,
+            toEqual: true,
+            actualResult: value.id
+        },
+        "The id should be a number": {
+            expect: typeof(value.id) === "number",
+            toEqual: true,
+            actualResult: value.id
+        },
+        "It should post a name": {
+            expect: value.name === "Zach and Ben",
+            toEqual: true,
+            actualResult: value.name
+        },
+        "The name should be a string": {
+            expect: typeof(value.name) === "string",
+            toEqual: true,
+            actualResult: value.name
+        },
+        "It should post an email": {
+            expect: value.email === "demo@cqc.com",
+            toEqual: true,
+            actualResult: value.email
+        },
+        "The email should be a string": {
+            expect: typeof(value.email) === "string",
+            toEqual: true,
+            actualResult: value.email
+        } // the email string should include one @ and one .
+    };
+
+    assertionLogger(assertions, reason, value);
+
     // then deletes email
     deleteEmail({PORT: ML_PORT, HOST: ML_HOST, ML_API_KEY})(
         function (value, reason) {
