b5c8feb40ed7d232550434f4ab7554d11e250004
Ben Pardo <visaudi@gmail.com>
2018-09-29 02:21:54 -0500

    Passes assertions into test.

--- tests/test-email-registration/expects-email-registered.mjs
+++ tests/test-email-registration/expects-email-registered.mjs
@@ -1,40 +1,9 @@
 import assertionLogger from "../assertion-logger";
 import deleteEmail from "../delete-email";
 
-export default function expectEmailRegistered(tearDown) {
+export default function expectEmailRegistered(tearDown, composeAssertions) {
     return function expectationRequestor (callback, value) {
-        const assertions = {
-            "It should generate an id": {
-                expect: value.id === 283325272034485980,
-                toEqual: true,
-                actualResult: value.id
-            },
-            "The id should be a number": {
-                expect: typeof(value.id) === "number",
-                toEqual: true,
-                actualResult: value.id
-            },
-            "It should post a name": {
-                expect: value.name === "Zach and Ben",
-                toEqual: true,
-                actualResult: value.name
-            },
-            "The name should be a string": {
-                expect: typeof(value.name) === "string",
-                toEqual: true,
-                actualResult: value.name
-            },
-            "It should post an email": {
-                expect: value.email === "demo@cqc.com",
-                toEqual: true,
-                actualResult: value.email
-            },
-            "The email should be a string": {
-                expect: typeof(value.email) === "string",
-                toEqual: true,
-                actualResult: value.email
-            }
-        }; 
+        const assertions = composeAssertions(value);
         const test = {
             assertions,
             successMessage: "All tests pass in file: api-post-request"
--- tests/test-email-registration/tests-email-registration.mjs
+++ tests/test-email-registration/tests-email-registration.mjs
@@ -17,7 +17,40 @@ function testEmailRegistration(environmentVariables, expectedPayload) {
         expectsEmailRegistered(deleteEmail(
             environmentVariables,
             expectedPayload
-        ))
+        ), function (value) {
+            return {
+                "It should generate an id": {
+                    expect: value.id === 283325272034485980,
+                    toEqual: true,
+                    actualResult: value.id
+                },
+                "The id should be a number": {
+                    expect: typeof(value.id) === "number",
+                    toEqual: true,
+                    actualResult: value.id
+                },
+                "It should post a name": {
+                    expect: value.name === "Zach and Ben",
+                    toEqual: true,
+                    actualResult: value.name
+                },
+                "The name should be a string": {
+                    expect: typeof(value.name) === "string",
+                    toEqual: true,
+                    actualResult: value.name
+                },
+                "It should post an email": {
+                    expect: value.email === "demo@cqc.com",
+                    toEqual: true,
+                    actualResult: value.email
+                },
+                "The email should be a string": {
+                    expect: typeof(value.email) === "string",
+                    toEqual: true,
+                    actualResult: value.email
+                }
+            };
+        })
     ])
 }
 
